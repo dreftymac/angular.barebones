28c0bf742fc0e9b7eabbcbcf587fb78c0432cf00
dreftymac <dreftymac@gmail.com>
2016-04-20 18:59:38 -0700

    local update ;; remove Origin from headers causes error

--- html/demo.postmethod.js
+++ html/demo.postmethod.js
@@ -45,7 +45,7 @@ angular.module('DemoApp', [])
         ,data: $httpParamSerializerJQLike($scope.appForm.data)
         ,headers: {'Accept-Language':'en'          
           ,'Content-Type': 'application/x-www-form-urlencoded'
-          ,'Origin': 'http://dreftymac.bounceme.net/'          
+          //,'Origin': 'http://dreftymac.bounceme.net/'          
           ,'Access-Control-Allow-Origin': '*'          
         }
       }).success(function(response) { console.log(response) });
