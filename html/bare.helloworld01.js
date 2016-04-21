74bca2f8da9f04df5593063348ea277c5a5abf38
dreftymac <dreftymac@gmail.com>
2016-04-21 09:20:35 -0700

    local update ;;  watchful_beamy_marian

---
+++ html/bare.helloworld01.js
@@ -0,0 +1,25 @@
+// <beg-file_info>
+// main:
+//   - date: created="Thu Apr 21 09:19:42 2016"
+//     last: lastmod="Thu Apr 21 09:19:42 2016"
+//     tags:   tags 
+//     author: created="__author__"
+//     dreftymacid:    "wizardly_fire_awful"
+//     filetype:       "yaml"
+//     seealso: |
+//         *
+//     desc: |
+//         ng script using host_least syntax style
+// <end-file_info>
+
+angular.module('helloworld01', [])
+
+ // Form directive
+.directive('appForm', function() {
+  return {
+    restrict:   'E',
+    scope:      {},
+    controller: 'AppFormCtrl',
+    templateUrl: 'demo.postmethod-appform.html'
+  };
+})
