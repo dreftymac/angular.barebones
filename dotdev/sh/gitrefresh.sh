7c02aa52de1ad245d9a9f1f98bae61d9626316ab
dreftymac <dreftymac@gmail.com>
2016-04-20 17:48:46 -0700

    local update ;;

--- dotdev/sh/gitrefresh.sh
+++ dotdev/sh/gitrefresh.sh
@@ -1,3 +1,16 @@
+### <beg-file_info>
+### main:
+###   - date: created="Wed Apr 20 17:46:48 2016"
+###     last: lastmod="Wed Apr 20 17:46:48 2016"
+###     tags: tags 
+###     dreftymacid: "fair_club_validly"
+###     filetype: "yaml"
+###     seealso: |
+###         *
+###     desc: |
+###         run this script to refresh git on both the local machine and the VM devbox
+### <end-file_info>
+
 echo "### ******************** "
 echo "## running $0"
 echo ""
