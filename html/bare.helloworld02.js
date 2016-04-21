20a8f49abb1983dc2e4b17806f0cdff4caeafb6a
dreftymac <dreftymac@gmail.com>
2016-04-21 12:44:14 -0700

    local update ;; devlog changes to helloworld02.html

--- html/bare.helloworld02.js
+++ html/bare.helloworld02.js
@@ -2,13 +2,13 @@
 // main:
 //   - date: created="Thu Apr 21 10:47:38 2016"
 //     last: lastmod="Thu Apr 21 10:47:38 2016"
-//     tags: tags 
+//     tags: tags
 //     dreftymacid: "volga_aims_adult"
 //     filetype: "yaml"
 //     seealso: |
 //         *
 //     desc: |
-//         * 
+//         *
 // <end-file_info>
 
 // ------------------------------------------------------------------------
@@ -18,19 +18,22 @@
 var moduleAlpha = angular.module("MyModuleAlpha", []);
 moduleAlpha.controller("MyControllerAlpha", function($scope) {
     $scope.name = "Alpha 123";
-});      
+    $scope.alpha1 = 'alpha1';
+});
 
 // Bravo
 var moduleBravo = angular.module("MyModuleBravo", []);
 moduleBravo.controller("MyControllerBravo", function($scope) {
     $scope.name = "Bravo 123";
-});      
+    $scope.bravo1 = 'bravo1';
+});
 
 // Charlie
 var moduleCharlie = angular.module("MyModuleCharlie", []);
 moduleCharlie.controller("MyControllerCharlie", function($scope) {
     $scope.name = "Charlie 123";
-});      
+    $scope.charlie1 = 'charlie1';
+});
 
 // ------------------------------------------------------------------------
 // begin_ declare parent_containing AppModule
@@ -39,7 +42,7 @@ angular.module("AppModule",[ "MyModuleAlpha"
      , "MyModuleCharlie"
 ])
 
-// filter -- string transform 
+// filter -- string transform
 .filter('exclaim',  function() {
   var vout = function(input) {return ['!!!',input,'!!!'].join(' ');};
   return vout;
