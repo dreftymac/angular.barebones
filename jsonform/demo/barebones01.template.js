f82e77593fecd86b368a399dc8f756c4e2f7f6a2
dreftymac <dreftymac@gmail.com>
2016-04-21 17:34:30 -0700

    local update ;; updates to devlog

--- jsonform/demo/barebones01.template.js
+++ jsonform/demo/barebones01.template.js
@@ -5,18 +5,6 @@
     "label":    "Basic Information",
     "fields": [
       {
-        "type": "hidden",
-        "model": "hidden",
-        "label": "hidden",
-        "val": "this is a secret"
-      },
-      {
-        "type":   "hidden",
-        "model":  "hidden2",
-        "label":  "hidden2",
-        "val":    "another secret"
-      },            
-      {
         "type": "legend",
         "model": "legend",
         "label": "Lorem ipsum yahoo <a href='http://www.yahoo.com'>yahoo</a> hyperlink supported."
@@ -26,60 +14,17 @@
         "model": "image",
         "label": "image",
         "source": "http://angularjs.org/img/AngularJS-large.png"
-      },      
-      {
-        "type": "month",
-        "model": "month",
-        "label": "month",
-        "placeholder": "month"
       },
       {
-        "type": "checklist",
-        "model": "nested.checklist",
-        "label": "nested.checklist",
-        "options": {
-          "first": {
-            "label": "first option"
-          },
-          "second": {
-            "label": "second option",
-            "isOn": "true",
-            "isOff": "false"
-          }
-        }
+        "type": "text",
+        "model": "fname01",
+        "label": "First Name"
       },
       {
-        "type": "number",
-        "model": "number",
-        "label": "number",
-        "placeholder": "number"
-      },
-      {
-        "type": "password",
-        "model": "password",
-        "label": "password",
-        "placeholder": "password"
-      },
-      {
-        "type": "radio",
-        "model": "radio",
-        "label": "radio",
-        "values": {
-        "first"  : "first option", 
-        "second" : "second option",
-        "third"  : "third option", 
-        "fourth" : "fourth option",
-        "fifth"  : "fifth option"  
-        }
-      },
-      {
-        "type": "range",
-        "model": "number",
-        "label": "range",
-        "val": 42,
-        "minValue": -42,
-        "maxValue": 84
-      }
+        "type": "text",
+        "model": "lname01",
+        "label": "Last Name"
+      },       
     ]
   }
-]  
+]
