50d0fad6725c2744838118b034acbeb6e6665f4b
dreftymac <dreftymac@gmail.com>
2016-04-21 19:40:30 -0700

    local update ;; updates to devlog

--- jsonform/demo/barebones01.template.js
+++ jsonform/demo/barebones01.template.js
@@ -5,14 +5,17 @@
     "label": "image",
     "source": "http://angularjs.org/img/AngularJS-large.png"
   }
+// ------------------------------------------------------------------------
+// begin_ 
   ,
   {
     "type":     "fieldset",
-    "model":    "fieldset",
+    "model":    "fieldset001",
     "label":    "Basic Information",
-    "fields": [{"type": "legend","model": "legend01","label":
+    "fields": [{"type": "legend","model": "legend001","label":
                "For more information about registration, please see the <a href='http://jsma.uoregon.edu/ArtCamp'>JSMA website</a>."}
-
+// ------------------------------------------------------------------------
+// begin_ 
       ,{
         "type": "text",
         "model": "pargfname01",
@@ -33,6 +36,41 @@
         "model": "studlname01",
         "label": "Student Last Name"
       }        
+// ------------------------------------------------------------------------
+// begin_ 
+  ,
+  {
+    "type":     "fieldset",
+    "model":    "fieldset002",
+    "label":    "Student Details",
+    "fields": [{"type":"legend" ,"model":"legend002", "label":
+               ""}
+// ------------------------------------------------------------------------
+// begin_ 
+      ,{
+        "type": "text",
+        "model": "addressline1_002",
+        "label": "Address Line 1"
+      }
+      ,{
+        "type": "text",
+        "model": "addressline2_002",
+        "val": "__blank__"
+        "label": "Address Line 2"
+      }
+      ,{
+        "type": "text",
+        "model": "studfname01",
+        "label": "Student First Name"
+      }
+      ,{
+        "type": "text",
+        "model": "studlname01",
+        "label": "Student Last Name"
+      }        
+// ------------------------------------------------------------------------
+// begin_ 
+
     ]
   }
 ]
