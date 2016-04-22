897de5d2fec9ba9e80ffbae140b9a7b5444015cb
dreftymac <dreftymac@gmail.com>
2016-04-21 17:35:37 -0700

    local update ;; updates to devlog

--- jsonform/demo/barebones01.template.js
+++ jsonform/demo/barebones01.template.js
@@ -3,28 +3,23 @@
     "type":     "fieldset",
     "model":    "fieldset",
     "label":    "Basic Information",
-    "fields": [
-      {
-        "type": "legend",
-        "model": "legend",
-        "label": "Lorem ipsum yahoo <a href='http://www.yahoo.com'>yahoo</a> hyperlink supported."
-      },
-      {
+    "fields": [{"type": "legend","model": "legend","label": "Lorem ipsum yahoo <a href='http://www.yahoo.com'>yahoo</a> hyperlink supported."}
+      ,{
         "type": "image",
         "model": "image",
         "label": "image",
         "source": "http://angularjs.org/img/AngularJS-large.png"
-      },
-      {
+      }
+      ,{
         "type": "text",
         "model": "fname01",
         "label": "First Name"
-      },
-      {
+      }
+      ,{
         "type": "text",
         "model": "lname01",
         "label": "Last Name"
-      },       
+      }   
     ]
   }
 ]
