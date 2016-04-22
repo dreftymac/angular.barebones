0d67d2e19ed197886bc47b62c937734b2e729ac0
dreftymac <dreftymac@gmail.com>
2016-04-21 17:06:25 -0700

    local update ;; updates to devlog

---
+++ jsonform/demo/barebones01.template.js
@@ -0,0 +1,79 @@
+[
+  {
+    "type": "fieldset",
+    "model": "fieldset",
+    "label": "fieldset",
+    "fields": [
+      {
+        "type": "hidden",
+        "model": "hidden",
+        "label": "hidden",
+        "val": "hidden"
+      },
+      {
+        "type": "image",
+        "model": "image",
+        "label": "image",
+        "source": "http://angularjs.org/img/AngularJS-large.png"
+      },
+      {
+        "type": "legend",
+        "model": "legend",
+        "label": "legend"
+      },
+      {
+        "type": "month",
+        "model": "month",
+        "label": "month",
+        "placeholder": "month"
+      },
+      {
+        "type": "checklist",
+        "model": "nested.checklist",
+        "label": "nested.checklist",
+        "options": {
+          "first": {
+            "label": "first option"
+          },
+          "second": {
+            "label": "second option",
+            "isOn": "on",
+            "isOff": "off"
+          }
+        }
+      },
+      {
+        "type": "number",
+        "model": "number",
+        "label": "number",
+        "placeholder": "number"
+      },
+      {
+        "type": "password",
+        "model": "password",
+        "label": "password",
+        "placeholder": "password"
+      },
+      {
+        "type": "radio",
+        "model": "radio",
+        "label": "radio",
+        "values": {
+          "first": "first option",
+          "second": "second option",
+          "third": "third option",
+          "fourth": "fourth option",
+          "fifth": "fifth option"
+        }
+      },
+      {
+        "type": "range",
+        "model": "number",
+        "label": "range",
+        "val": 42,
+        "minValue": -42,
+        "maxValue": 84
+      }
+    ]
+  }
+]  
